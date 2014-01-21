6abb5f04286fe1dba523485e1ce13a2e1349b1ae
松山　大樹 <2011531060@ma218.is.env.kitakyu-u.ac.jp>
2014-01-21 15:13:54 +0900

    git@github.com:orion468/dentaku.git

--- newdentaku/ViewController.h
+++ newdentaku/ViewController.h
@@ -8,6 +8,24 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    IBOutlet UILabel *Result;
+}
+- (IBAction)Btn0:(id)sender;
+- (IBAction)Btn1:(id)sender;
+- (IBAction)Btn2:(id)sender;
+- (IBAction)Btn3:(id)sender;
+- (IBAction)Btn4:(id)sender;
+- (IBAction)Btn5:(id)sender;
+- (IBAction)Btn6:(id)sender;
+- (IBAction)Btn7:(id)sender;
+- (IBAction)Btn8:(id)sender;
+- (IBAction)Btn9:(id)sender;
+- (IBAction)AllClearBtn:(id)sender;
+- (IBAction)TasuBtn:(id)sender;
+- (IBAction)HikuBtn:(id)sender;
+- (IBAction)KakeruBtn:(id)sender;
+- (IBAction)WaruBtn:(id)sender;
+- (IBAction)EqualBtn:(id)sender;
 
 @end
--- newdentaku/ViewController.m
+++ newdentaku/ViewController.m
@@ -8,6 +8,10 @@
 
 #import "ViewController.h"
 
+int Method;
+int SelectNumber;
+float RunningTotal;
+
 @interface ViewController ()
 
 @end
@@ -24,6 +28,195 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)Btn0:(id)sender {
+    
+    SelectNumber = SelectNumber *10;
+    SelectNumber = SelectNumber +0;
+    Result.text = [NSString stringWithFormat:@"%i", SelectNumber];
+}
+
+- (IBAction)Btn1:(id)sender {
+    SelectNumber = SelectNumber *10;
+    SelectNumber = SelectNumber +1;
+    Result.text = [NSString stringWithFormat:@"%i", SelectNumber];
+
+}
+
+- (IBAction)Btn2:(id)sender {
+    
+    SelectNumber = SelectNumber *10;
+    SelectNumber = SelectNumber +2;
+    Result.text = [NSString stringWithFormat:@"%i", SelectNumber];
+}
+
+- (IBAction)Btn3:(id)sender {
+    
+    SelectNumber = SelectNumber *10;
+    SelectNumber = SelectNumber +3;
+    Result.text = [NSString stringWithFormat:@"%i", SelectNumber];
+}
+
+- (IBAction)Btn4:(id)sender {
+    SelectNumber = SelectNumber *10;
+    SelectNumber = SelectNumber +4;
+    Result.text = [NSString stringWithFormat:@"%i", SelectNumber];
+
+}
+
+- (IBAction)Btn5:(id)sender {
+    
+    SelectNumber = SelectNumber *10;
+    SelectNumber = SelectNumber +5;
+    Result.text = [NSString stringWithFormat:@"%i", SelectNumber];
+}
+
+- (IBAction)Btn6:(id)sender {
+    SelectNumber = SelectNumber *10;
+    SelectNumber = SelectNumber +6;
+    Result.text = [NSString stringWithFormat:@"%i", SelectNumber];
+
+}
+
+- (IBAction)Btn7:(id)sender {
+    
+    SelectNumber = SelectNumber *10;
+    SelectNumber = SelectNumber +7;
+    Result.text = [NSString stringWithFormat:@"%i", SelectNumber];
+}
+
+- (IBAction)Btn8:(id)sender {
+    SelectNumber = SelectNumber *10;
+    SelectNumber = SelectNumber +8;
+    Result.text = [NSString stringWithFormat:@"%i", SelectNumber];
+
+}
+
+- (IBAction)Btn9:(id)sender {
+    SelectNumber = SelectNumber *10;
+    SelectNumber = SelectNumber +9;
+    Result.text = [NSString stringWithFormat:@"%i", SelectNumber];
+
+}
+
+- (IBAction)AllClearBtn:(id)sender {
+    SelectNumber = 0;
+    Method = 0;
+    RunningTotal = 0;
+    
+    Result.text = [NSString stringWithFormat:@"0"];
+}
+
+- (IBAction)TasuBtn:(id)sender {
+    if (RunningTotal == 0) {
+        RunningTotal = SelectNumber;
+    }
+    
+    if (Method == 4) {
+        RunningTotal = RunningTotal + SelectNumber;
+    }
+    if (Method == 3) {
+        RunningTotal = RunningTotal - SelectNumber;
+    }
+    if (Method == 2) {
+        RunningTotal = RunningTotal * SelectNumber;
+    }
+    if (Method == 1) {
+        RunningTotal = RunningTotal / SelectNumber;
+    }
+    Method = 4;
+    SelectNumber = 0;
+}
+
+- (IBAction)HikuBtn:(id)sender {
+    if (RunningTotal == 0) {
+        RunningTotal = SelectNumber;
+    }
+    if (Method == 4) {
+        RunningTotal = RunningTotal + SelectNumber;
+    }
+    if (Method == 3) {
+        RunningTotal = RunningTotal - SelectNumber;
+    }
+    if (Method == 2) {
+        RunningTotal = RunningTotal * SelectNumber;
+    }
+    if (Method == 1) {
+        RunningTotal = RunningTotal / SelectNumber;
+    }
+    Method = 3;
+    SelectNumber = 0;
+}
+
+- (IBAction)KakeruBtn:(id)sender {
+    if (RunningTotal == 0) {
+        RunningTotal = SelectNumber;
+    }
+    if (Method == 4) {
+        RunningTotal = RunningTotal + SelectNumber;
+    }
+    if (Method == 3) {
+        RunningTotal = RunningTotal - SelectNumber;
+    }
+    if (Method == 2) {
+        RunningTotal = RunningTotal * SelectNumber;
+    }
+    if (Method == 1) {
+        RunningTotal = RunningTotal / SelectNumber;
+    }
+    Method = 2;
+    SelectNumber = 0;
+}
+
+- (IBAction)WaruBtn:(id)sender {
+    if (RunningTotal == 0) {
+        RunningTotal = SelectNumber;
+    }
+    if (Method == 4) {
+        RunningTotal = RunningTotal + SelectNumber;
+    }
+    if (Method == 3) {
+        RunningTotal = RunningTotal - SelectNumber;
+    }
+    if (Method == 2) {
+        RunningTotal = RunningTotal * SelectNumber;
+    }
+    if (Method == 1) {
+        RunningTotal = RunningTotal / SelectNumber;
+    }
+    Method = 1;
+    SelectNumber = 0;
+}
+
+- (IBAction)EqualBtn:(id)sender {
+    if (RunningTotal == 0) {
+        RunningTotal = SelectNumber;
+    }
+    else{
+        
+        switch (Method) {
+            case 1:
+                RunningTotal = RunningTotal / SelectNumber;
+                break;
+            case 2:
+                RunningTotal = RunningTotal * SelectNumber;
+                break;
+            case 3:
+                RunningTotal = RunningTotal - SelectNumber;
+                break;
+            case 4:
+                RunningTotal = RunningTotal + SelectNumber;
+                break;
+                
+                default:
+                break;
+        }
+    }
+    Method = 0;
+    SelectNumber = 0;
+    
+    Result.text = [NSString stringWithFormat:@"%.f",RunningTotal];
 }
 
 @end
